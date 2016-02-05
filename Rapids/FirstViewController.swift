f4ba443b8cfb7003723005c7fc9fd91aca8b24ff
Noah <noah@node-1w7jr9qldkkaszf6voar89zn4.ipv6.telus.net>
2016-02-04 18:11:29 -0800

    Initial Commit

---
+++ Rapids/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Rapids
+//
+//  Created by Noah on 2016-02-04.
+//  Copyright © 2016 Riverside Secondary School. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
