3f5374ec7d0bcb03a2a2411a72dca13071716ee2
mac craig <maccraig98@gmail.com>
2016-02-15 11:11:38 -0800

    Setup storyboards

---
+++ Rapids/AnnouncementsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AnnouncementsViewController.swift
+//  Rapids
+//
+//  Created by Mac Craig on 2016-02-15.
+//  Copyright © 2016 Riverside Secondary School. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class AnnouncementsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
