bd31ce0bd093db874b607dfe097925da8612bad2
Noah <noah@132-112-M-5.local>
2016-02-16 15:17:16 -0800

    Setup CocoaPods to support using frameworks
    Also adds the Alamofire library and preparations for the networking layer

---
+++ Pods/Target Support Files/Pods-Rapids/Pods-Rapids-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rapids : NSObject
+@end
+@implementation PodsDummy_Pods_Rapids
+@end
---
+++ Pods/Target Support Files/Pods-RapidsTests/Pods-RapidsTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RapidsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RapidsTestsVersionString[];
+
