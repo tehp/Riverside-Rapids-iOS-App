deb88ff4ff1c67cefe4eef6c57b2c3a534865d75
mac craig <maccraig98@gmail.com>
2016-02-17 11:46:35 -0800

    More page UI setup

--- Rapids/MoreViewController.swift
+++ Rapids/MoreViewController.swift
@@ -14,6 +14,9 @@ class MoreViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
