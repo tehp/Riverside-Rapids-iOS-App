2d80d6d56074554a324569b9882cdadabb75f0b9
Noah Tajwar <noah@sewlochan.com>
2016-02-25 11:32:33 -0800

    Setup file for URL Constants etc.

---
+++ Rapids/D.swift
@@ -0,0 +1,21 @@
+//
+//  D.swift
+//  Rapids
+//
+//  Created by Programming on 2016-02-25.
+//  Copyright © 2016 Riverside Secondary School. All rights reserved.
+//
+
+import Foundation
+
+class D {
+    
+    struct SharePoint {
+        
+    }
+    
+    struct BellSchedule {
+        
+    }
+    
+}
