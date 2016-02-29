41a20e6012be7ea3c11e848c756c348b36dd9071
Noah Tajwar <noah@sewlochan.com>
2016-02-29 11:14:39 -0800

    Added BlockTableViewCell file

---
+++ Rapids/Views/BlockTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BlockTableViewCell.swift
+//  Rapids
+//
+//  Created by Programming on 2016-02-29.
+//  Copyright © 2016 Riverside Secondary School. All rights reserved.
+//
+
+import UIKit
+
+class BlockTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
