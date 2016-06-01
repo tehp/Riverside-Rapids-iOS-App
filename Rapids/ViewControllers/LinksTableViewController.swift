f87619f78cf1fc39b3d64707a73d583f0a2c6107
Shaun Tyers <shauntyers@gmail.com>
2016-06-01 11:47:23 -0700

    Added constraints to the links page labels

--- Rapids/ViewControllers/LinksTableViewController.swift
+++ Rapids/ViewControllers/LinksTableViewController.swift
@@ -11,16 +11,13 @@ import UIKit
 class LinksTableViewController: UITableViewController {
 
     let linkNames: [String] = ["Riverside Homepage", "Riverside Intranet", "Twitter (@rside43)", "The Eddy", "Gym Schedule", "Principal's Blog"]
-    let linkUrls: [String] = ["http://www.sd43.bc.ca/secondary/riverside/Pages"]
+    let linkUrls: [String] = ["http://www.sd43.bc.ca/secondary/riverside/Pages/default.aspx", "https://my43.sd43.bc.ca/schools/riverside/Pages/Default.aspx", "https://twitter.com/rside43", "http://www.riversideeddy.ca", "http://www.sd43.bc.ca/secondary/riverside/ProgramsServices/Sports/Pages/default.aspx", "http://aciolfitto.wordpress.com/"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,6 +39,10 @@ class LinksTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("LinkTableViewCell", forIndexPath: indexPath) as! LinkTableViewCell
+        
+        let row = indexPath.row
+        cell.linkNameLabel.text = linkNames[row]
+        cell.linkUrlLabel.text = linkUrls[row]
         
         return cell
     }
